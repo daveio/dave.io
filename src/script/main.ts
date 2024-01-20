255efe8c11003692ba1f6816615f5f29c9d69bae
Dave Williams <dave@dave.io>
2024-01-20 14:23:11 +0000

    sync

---
+++ src/script/main.ts
@@ -0,0 +1,13 @@
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/pro-solid-svg-icons'
+// import { far } from '@fortawesome/pro-regular-svg-icons'
+// import { fal } from '@fortawesome/pro-light-svg-icons'
+// import { fat } from '@fortawesome/pro-thin-svg-icons'
+// import { fad } from '@fortawesome/pro-duotone-svg-icons'
+// import { fass } from '@fortawesome/sharp-solid-svg-icons'
+// import { fasr } from '@fortawesome/sharp-regular-svg-icons'
+// import { fasl } from '@fortawesome/sharp-light-svg-icons'
+// library.add(fas, far, fal, fat, fad, fass, fasr, fasl)
+import('@fortawesome/fontawesome-svg-core')
+import('@fortawesome/free-brands-svg-icons')
+import('@fortawesome/sharp-light-svg-icons')
