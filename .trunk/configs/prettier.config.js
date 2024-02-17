bafd6835bd7551c8d46c2b0646136660bad4b32c
Dave Williams <dave@dave.io>
2024-02-17 02:07:20 +0000

    use symlinks for trunk configs

--- .trunk/configs/prettier.config.js
+++ .trunk/configs/prettier.config.js
@@ -1,17 +1,1 @@
-module.exports = {
-  plugins: [
-    'prettier-plugin-tailwindcss',
-    'prettier-plugin-css-order',
-    'prettier-plugin-import-sort',
-    'prettier-plugin-jsdoc',
-    'prettier-plugin-organize-attributes',
-    'prettier-plugin-organize-imports',
-    'prettier-plugin-style-order',
-    'prettier-plugin-sort-imports',
-  ],
-  tailwindConfig: './tailwind.config.js',
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
-}
+../../prettier.config.js
