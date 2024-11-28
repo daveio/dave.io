e2e3aa049988b5247438a2e448cf34982609d57b
Dave Williams <dave@dave.io>
2024-11-28 21:34:11 +0000

    prep for vue designer

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    liveDesigner({}),
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
+      '~~': fileURLToPath(new URL('./', import.meta.url)),
     },
   },
 })
