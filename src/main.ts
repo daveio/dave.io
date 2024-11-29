62c50ab6128fb62df8b94a9e4bb5d295211dcf30
Dave Williams <dave@dave.io>
2024-11-29 01:46:44 +0000

    GA with vue-gtag

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './css/main.css'
+import VueGtag from "vue-gtag"
 
-createApp(App).mount('#app')
+createApp(App).use(VueGtag, {
+  config: { id: "G-5WNQT8CW9J" }
+}).mount("#app")
