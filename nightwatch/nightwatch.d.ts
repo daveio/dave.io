a324ae32ca486dc9b6e013017f1d001d175aa530
Dave Williams <dave@dave.io>
2024-11-29 15:05:34 +0000

    add placeholders for trunk

--- nightwatch/nightwatch.d.ts
+++ nightwatch/nightwatch.d.ts
@@ -3,11 +3,13 @@ import { NightwatchCustomAssertions, NightwatchCustomCommands } from 'nightwatch
 declare module 'nightwatch' {
   interface NightwatchCustomAssertions {
     // Add your custom assertions' types here
+    placeholderAssertion: (selector: string, count: number) => NightwatchBrowser
     // elementHasCount: (selector: string, count: number) => NightwatchBrowser
   }
 
   interface NightwatchCustomCommands {
     // Add your custom commands' types here
     // strictClick: (selector: string) => NightwatchBrowser
+    placeholderCommand: (selector: string) => NightwatchBrowser
   }
 }
