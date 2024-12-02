0131063ed29f72e12e245043cb2f938955ed4d7b
Dave Williams <dave@dave.io>
2024-12-02 00:40:38 +0000

    merge changes from old back in, remove old

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="pt-3 flex flex-row space-x-5">
+    <h1><RouterLink to="/">about</RouterLink></h1>
+    <h1><RouterLink to="/gender">gender</RouterLink></h1>
+    <h1><RouterLink to="/cv">cv / resumé</RouterLink></h1>
   </header>
-
-  <RouterView />
+  <main class="flex justify-center pt-6">
+    <div class="size-full ml-6 mr-9 min-h-96 bg-black">
+      <RouterView />
+      <p class="min-h-full">boop</p>
+      <p>barf</p>
+    </div>
+  </main>
+  <footer class="mt-auto pb-3">
+    <h2>All junk copyright literally nobody.</h2>
+    <h2>
+      Feel free to yoink any of
+      <a href="https://github.com/daveio/dave.io">the code</a> if it helps you.
+    </h2>
+    <h2>
+      Just do me a favour and
+      <a href="https://github.com/daveio/dave.io/issues/new">submit a ticket</a>
+      telling me what you make?
+    </h2>
+  </footer>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -81,5 +90,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
