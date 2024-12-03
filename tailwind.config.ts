dfdb9b865f647261d3d5c7ede62c7cc552c652cc
Dave Williams <dave@dave.io>
2024-12-03 16:50:42 +0000

    pinegrow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,17 @@
 import type { Config } from 'tailwindcss'
+import { pg_colors, pg_fonts, pg_backgrounds } from './themes/pg-tailwindcss/tokens.mjs'
 
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  get content() {
+    const _content = [
+      './index.html',
+      './src/**/*.{html,vue,svelte,astro,js,cjs,mjs,ts,cts,mts,jsx,tsx,md,mdx}',
+      //...
+    ]
+    return process.env.NODE_ENV === 'production'
+      ? _content
+      : [..._content, './_pginfo/**/*.{html,js}'] // used by Vue Designer Desginer for live-designing during development
+  },
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
@@ -1060,5 +1070,12 @@ export default {
       50: '50',
     },
   },
-  plugins: [],
+  plugins: [
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require('@pinegrow/tailwindcss-plugin').default({
+      colors: pg_colors, // primary, secondary etc
+      fonts: pg_fonts,
+      backgrounds: pg_backgrounds, // bg-design-image, bg-design-image-large
+    }),
+  ],
 } satisfies Config
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { configDefaults } from 'vitest/config'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,13 +6,30 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
+import liveDesigner from '@pinegrow/vite-plugin'
 
-// https://vite.dev/config/
+// https:/ / vite.dev / config /
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), nightwatchPlugin()],
+  plugins: [
+    liveDesigner({
+      tailwindcss: {
+        configPath: 'tailwind.config.js',
+        cssPath: '@/assets/css/tailwind.css',
+        restartOnConfigUpdate: true,
+        restartOnThemeUpdate: true,
+        // themePath: false, // disable Design Panel
+      },
+    }),
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    nightwatchPlugin(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
+      '~~': fileURLToPath(new URL('./', import.meta.url)),
     },
   },
 })
