98b1b2108839620965698a9ba5dcbf14d4fe3433
Dave Williams <dave@dave.io>
2024-12-03 20:12:48 +0000

    update theme

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,27 @@ import { RouterLink, RouterView } from 'vue-router'
 import BaseIcon from '@/components/BaseIcon.vue'
 </script>
 <template>
-    <header class="pt-3 flex flex-row space-x-5">
-        <h1><RouterLink to="/">about</RouterLink></h1>
-        <h1><RouterLink to="/gender">gender</RouterLink></h1>
-        <h1><RouterLink to="/cv">cv / resumé</RouterLink></h1>
+  <div class="flex flex-col">
+    <header class="flex flex-row space-x-5">
+      <h1><BaseIcon />&nbsp;<RouterLink to="/">about</RouterLink></h1>
+      <h1><BaseIcon />&nbsp;<RouterLink to="/gender">gender</RouterLink></h1>
+      <h1><BaseIcon />&nbsp;<RouterLink to="/cv">cv / resumé</RouterLink></h1>
     </header>
     <main class="flex justify-center pt-6">
-        <div class="size-full min-h-full ml-6 mr-9 bg-black">
-            <BaseIcon name="i-material-symbols-10k"/>
-            <RouterView/>
-        </div>
+      <RouterView />
     </main>
     <footer class="mt-auto pb-3">
-        <h2>All junk copyright literally nobody.</h2>
-        <h2>
-      Feel free to yoink any of <a href="https://github.com/daveio/dave.io">the code</a> if it helps
-      you. </h2>
-        <h2>
-      Just do me a favour and <a href="https://github.com/daveio/dave.io/issues/new">submit a ticket</a> telling me what you
-      make? </h2>
+      <h2>All junk copyright literally nobody.</h2>
+      <h2>
+        Feel free to yoink any of <a href="https://github.com/daveio/dave.io">the code</a> if it
+        helps you.
+      </h2>
+      <h2>
+        Just do me a favour and
+        <a href="https://github.com/daveio/dave.io/issues/new">submit a ticket</a> telling me what
+        you make?
+      </h2>
     </footer>
+  </div>
 </template>
 <style scoped></style>
