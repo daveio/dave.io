8c72937720f6fac8e2efa6e624c53ca654057b88
Dave Williams <dave@dave.io>
2024-12-07 19:05:04 +0000

    add trunk linters, fix all problems

--- src/components/__tests__/CounterButton.spec.ts
+++ src/components/__tests__/CounterButton.spec.ts
@@ -1,11 +1,11 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from "vitest";
 
-import { mount } from '@vue/test-utils'
-import CounterButton from '../CounterButton.vue'
+import { mount } from "@vue/test-utils";
+import CounterButton from "../CounterButton.vue";
 
-describe('Counter Button', () => {
-  it('renders properly', () => {
-    const wrapper = mount(CounterButton, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
-})
+describe("Counter Button", () => {
+	it("renders properly", () => {
+		const wrapper = mount(CounterButton, { props: { msg: "Hello Vitest" } });
+		expect(wrapper.text()).toContain("Hello Vitest");
+	});
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,49 @@
-import { configDefaults } from 'vitest/config'
-import { fileURLToPath, URL } from 'node:url'
+import { configDefaults } from "vitest/config";
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import nightwatchPlugin from 'vite-plugin-nightwatch'
-import Unocss from 'unocss/vite'
-import presetIcons from '@unocss/preset-icons'
-import { liveDesigner } from '@pinegrow/vite-plugin'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueDevTools from "vite-plugin-vue-devtools";
+import nightwatchPlugin from "vite-plugin-nightwatch";
+import Unocss from "unocss/vite";
+import presetIcons from "@unocss/preset-icons";
+import { liveDesigner } from "@pinegrow/vite-plugin";
 
 // https:/ / vite.dev / config /
 export default defineConfig({
-  plugins: [
-    liveDesigner({
-      tailwindcss: {
-        configPath: 'tailwind.config.ts',
-        cssPath: '@/assets/css/tailwind.css',
-        restartOnConfigUpdate: true,
-        restartOnThemeUpdate: true,
-      },
-    }),
-    Unocss({
-      presets: [
-        presetIcons({
-          prefix: 'i-', // default prefix, do not change
-        }),
-      ],
-      content: {
-        pipeline: {
-          /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
-          include: ['./src/**/*'],
-        },
-      },
-    }),
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    nightwatchPlugin(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': fileURLToPath(new URL('./src', import.meta.url)),
-      '~~': fileURLToPath(new URL('./', import.meta.url)),
-    },
-  },
-})
+	plugins: [
+		liveDesigner({
+			tailwindcss: {
+				configPath: "tailwind.config.ts",
+				cssPath: "@/assets/css/tailwind.css",
+				restartOnConfigUpdate: true,
+				restartOnThemeUpdate: true,
+			},
+		}),
+		Unocss({
+			presets: [
+				presetIcons({
+					prefix: "i-", // default prefix, do not change
+				}),
+			],
+			content: {
+				pipeline: {
+					/* Please ensure that you update the filenames and paths to accurately match those used in your project. */
+					include: ["./src/**/*"],
+				},
+			},
+		}),
+		vue(),
+		vueJsx(),
+		vueDevTools(),
+		nightwatchPlugin(),
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+			"~": fileURLToPath(new URL("./src", import.meta.url)),
+			"~~": fileURLToPath(new URL("./", import.meta.url)),
+		},
+	},
+});
